1c012534e14ad175f47fef7bb421696ac59fcd54
Reimnop <rayngu2005@gmail.com>
2022-07-01 19:59:29 +0700

    Bumped version

--- CatalystBase.cs
+++ CatalystBase.cs
@@ -14,7 +14,7 @@ public class CatalystBase : BaseUnityPlugin
     
     public const string Guid = "me.reimnop.catalyst";
     public const string Name = "Catalyst";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
 
     public const string Description = "Next-generation performance mod for Project Arrhythmia - Successor of Potassium";
 
